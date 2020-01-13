8d44b6f4b1a5cd089ed1d675a5758954f08b40b8
Matias Hernandez <matiashernandez@192.168.1.7>
2020-01-13 18:11:03 -0300

    Typography definitions progress.

---
+++ tasks/assets.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const { task, src, dest } = require('gulp');
+const rename = require('gulp-rename');
+const distribution_folder = path.join('.', 'docs', 'kss-assets');
+const assets_list = [
+  path.join('.', 'static', 'fonts.css')
+];
+
+task('assets', () => {
+	return src(assets_list)
+		.pipe(dest(distribution_folder, { "mode": "0777" }));
+});
